846e6399286ca3915bca4b4cd50d627e48ece3d5
vercerry07 <vito70156@gmail.com>
2021-09-29 11:30:41 +0530

    commit

--- client/src/component/screen/productscreen.js
+++ client/src/component/screen/productscreen.js
@@ -23,7 +23,46 @@ const Productscreen = ({match}) => {
     <Col md={3}>
     <ListGroup variant='flush'>
      <ListGroup.Item> <h2> {product.name} </h2></ListGroup.Item>   
+     <ListGroup.Item> <Rating value={product.rating} text={`${product.numReviews} reviews`} /> </ListGroup.Item> 
+     <ListGroup.Item>Price ${product.price}</ListGroup.Item>
+     <ListGroup.Item>Description {product.description}</ListGroup.Item>
+      
     </ListGroup>
+    </Col>
+    <Col md={3}>
+
+     <Card>
+         
+     <ListGroup variant='flush'> 
+      <ListGroup.Item>
+       <Row>
+
+        <Col>
+        price 
+       </Col>
+       <Col>
+         
+      <strong>{product.price}</strong>
+       </Col>   
+        </Row>    
+       </ListGroup.Item>   
+
+       <ListGroup.Item>
+       <Row>
+
+        <Col>
+        Status: 
+       </Col>
+       <Col>
+        {product.countInStock > 0 ? 'in stock':'out of stock'}
+       </Col>   
+        </Row>    
+       </ListGroup.Item>
+       <ListGroup.Item>
+        <Button className='btn-block' type='button' disabled={product.countInStock === 0}>Add to cart</Button>
+        </ListGroup.Item> 
+    </ListGroup> 
+     </Card>  
     </Col>
     </Row>   
  
