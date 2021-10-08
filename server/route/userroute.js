9d676a809d72a803a7a7b0d8edaabb8228015409
vercerry07 <vito70156@gmail.com>
2021-10-08 13:34:29 +0530

    commit

--- server/route/userroute.js
+++ server/route/userroute.js
@@ -6,11 +6,12 @@ let User = require('../model/usermodel')
 let asynchandler = require('express-async-handler')
 let generatetoken = require('../util/token')
 let protect = require('../middleware/authmiddleware')
+let bcrypt = require('bcryptjs')
 userroute.get('/',(req,res)=>{
+
   res.send('hello')
 
 })
-
 userroute.post('/login',asynchandler( async(req,res)=>{
   let {email, password} = req.body  
   let user = await User.findOne({email})
@@ -70,11 +71,11 @@ userroute.post('/', async(req,res)=>{
   let { name, email ,password} = req.body  
   let exuser = await User.findOne({email})
   if(!exuser){
-    
+        let pwd = await bcrypt.hash(password, 10)
         let ruser = await User({
          name,
          email,
-         password
+         password:pwd
         })
    
       if(ruser){
