f51bee52a95e602c805083aaa23afb7c36896687
vercerry07 <vito70156@gmail.com>
2021-10-08 10:40:30 +0530

    commit

--- server/route/userroute.js
+++ server/route/userroute.js
@@ -5,27 +5,29 @@ let userroute = express.Router()
 let User = require('../model/usermodel')
 let asynchandler = require('express-async-handler')
 let generatetoken = require('../util/token')
+let protect = require('../middleware/authmiddleware')
 userroute.get('/',(req,res)=>{
   res.send('hello')
+
 })
 
 userroute.post('/login',asynchandler( async(req,res)=>{
-
   let {email, password} = req.body  
   let user = await User.findOne({email})
+ 
+ 
+ 
+ 
   if(user && (await user.matchpassword(password))){
     
-    
-
-
    res.json({
-
     _id:user._id, 
     name:user.name,
     email:user.email,
     isAdmin:user.isAdmin,
     token: generatetoken(user._id)
-   })
+  
+  })
   
 } 
 
@@ -37,4 +39,66 @@ else {
 }))
 
 
+userroute.get('/profile', protect,async(req,res)=>{
+
+    
+  
+  let user = await User.findById(req.user._id) 
+  if(user){
+    res.json({
+      _id:user._id, 
+      name:user.name,
+      email:user.email,
+      isAdmin:user.isAdmin,
+      token: generatetoken(user._id)
+    })  
+  
+  
+  }
+  else {
+
+    res.status(401)
+    throw new Error('user not found')
+  }
+  res.send('hello')     
+})
+
+
+
+userroute.post('/', async(req,res)=>{
+
+  let { name, email ,password} = req.body  
+  let exuser = await User.findOne({email})
+  if(!exuser){
+    
+        let ruser = await User({
+         name,
+         email,
+         password
+        })
+   
+      if(ruser){
+       res.status(201).json({
+        _id:user._id, 
+        name:user.name,
+        email:user.email,
+        isAdmin:user.isAdmin,
+        token: generatetoken(user._id)
+       }) 
+      
+      
+      }
+      else {
+        res.status(400)
+        throw new Error('user can not be saved')
+      }
+    
+} 
+
+else {
+  res.status(400)
+  throw new Error('user exists')
+  
+}      
+})
 module.exports = userroute
